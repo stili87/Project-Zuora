2b0db954b72ad3a280111f2b71609e0d0bdb6c6b
Andrew Stilinovic <stili87@gmail.com>
2022-05-03 23:06:09 -0500

    skeleton completed

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const db = require('./models');
+const db = require('./db/models');
 const app = express();
-// const userRouter = require('./routes/users.js'); //routes to be updated
-// const postRouter = require('./routes/posts.js'); // routes to be updated
+const morgan = require('morgan');
+const routes = require('./routes');
+
+app.use(morgan('dev'));
 app.set('view engine', 'pug')
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
+app.use(routes)
 
 
 
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+    const err = new Error('The requested page couldn\'t be found.');
+    err.status = 404;
+    next(err);
+  });
+  
+  // Custom error handlers.
+  
+  // Error handler to log errors.
+  app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+      // TODO Log the error to the database.
+    } else {
+      console.error(err);
+    }
+    next(err);
+  });
+  
+  // Error handler for 404 errors.
+  app.use((err, req, res, next) => {
+    if (err.status === 404) {
+      res.status(404);
+      res.render('page-not-found', {
+        title: 'Page Not Found',
+      });
+    } else {
+      next(err);
+    }
+  });
+  
+  // Generic error handler.
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+      title: 'Server Error',
+      message: isProduction ? null : err.message,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
-
-const port = 8081;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+module.exports = app;
