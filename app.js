dee48a4dce264331e5e24efb814a801f4728cd2c
Andrew Stilinovic <stili87@gmail.com>
2022-05-04 12:40:18 -0500

    added express.json()

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'pug')
 app.use(express.static('./public'))
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
+app.use(express.json())
 app.use(routes)
 
 
