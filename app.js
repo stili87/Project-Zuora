9d9245d0e15a2dbb4fbc9ccfabe96b50fff8e272
Andrew Stilinovic <stili87@gmail.com>
2022-05-09 12:35:12 -0500

    added more skeleton code

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const db = require('./db/models');
 const morgan = require('morgan');
+const { sequelize } = require('./db/models');
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const { sessionSecret, environment } = require('./config');
 const routes = require('./routes');
-const app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const {restoreUser} = require('./auth.js')
+const app = express();
+
 
 app.use(morgan('dev'));
 app.set('view engine', 'pug')
-app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cookieParser(sessionSecret))
+const store = new SequelizeStore({ db: sequelize });
 app.use(session({
   name:'zuora.sid',
   secret: sessionSecret,
+  store,
   resave: false,
   saveUninitialized: false
 }))
 
+
+
+store.sync();
 //app.use(restoreUser)   turn on when auth part is added to routes
 //////PUT ALL ROUTERS HERE
-app.use(routes)
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 
 
@@ -60,14 +73,14 @@ app.use((req, res, next) => {
   });
   
   // Generic error handler.   NEED CUSTOM PUG PAGE FOR THIS.
-  app.use((err, req, res, next) => {
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
     res.status(err.status || 500);
-    const isProduction = process.env.NODE_ENV === 'production';
-    res.render('error', {
-      title: 'Server Error',
-      message: isProduction ? null : err.message,
-      stack: isProduction ? null : err.stack,
-    });
+    res.render('error');
   });
 
 module.exports = app;
