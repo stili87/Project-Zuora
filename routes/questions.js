98d4744430bb57bdd489668d5aadf275cb0d32b2
Waseemalame <waseemalame45@gmail.com>
2022-05-09 15:28:14 -0700

    questions route

--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../db/models')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const questions = db.Question.findAll();
+  console.log(questions)
+  res.send('questions', {
+    title: 'Questions',
+    questions
+   });
 });
 
 module.exports = router;
