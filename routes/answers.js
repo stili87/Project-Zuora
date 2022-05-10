308ef5a26136389d8c981596ba0285d25f35f6f3
jkintner25 <jkintner25@gmail.com>
2022-05-10 15:42:32 -0500

    finished delete answer route

--- routes/answers.js
+++ routes/answers.js
@@ -14,7 +14,7 @@ const checkAnswerFields = [
   check('streetCred')
     .isLength({ max: 200 })
     .withMessage('Too many credentials!')
-]
+];
 
 /* GET answers form for specific question. */
 router.get('/questions/:id/answers', requireAuth, csrfProtection,
@@ -24,7 +24,7 @@ router.get('/questions/:id/answers', requireAuth, csrfProtection,
     // const answer = Answer.build();
     res.render('answers-form', { id: questionId, csrfToken: req.csrfToken() })
   }
-));
+  ));
 
 router.post('/questions/:id/answers', requireAuth, checkAnswerFields, csrfProtection,
   asyncHandler(async function (req, res, next) {
@@ -42,25 +42,48 @@ router.post('/questions/:id/answers', requireAuth, checkAnswerFields, csrfProtec
       const errors = validatorErrors.array().map((error) => error.msg);
       res.render('answers-form', { id: questionId, errors, csrfToken: req.csrfToken() })
     }
-  }
-))
+  })
+);
 
-router.get('/questions/:questionId/answers/:answerId', requireAuth, checkAnswerFields, csrfProtection,
+router.get('/questions/:questionId/answers/:answerId', requireAuth, csrfProtection,
   asyncHandler(async function (req, res, next) {
     const { questionId, answerId } = req.params;
     const userId = req.session.auth.userId;
-    let answer = await Answer.findByPk(answerId);
+    const answer = await Answer.findByPk(answerId);
     if (userId === answer.userId) {
-      res.render('answer-edit', { questionId, answerId, answer, csrfToken: req.csrfToken() })
+      res.render('answer-edit', { questionId, answerId, answer, csrfToken: req.csrfToken() });
     } else {
-      res.redirect('/')
+      res.redirect('/');
     }
-  }
-))
+  })
+);
 
 router.post('/questions/:questionId/answers/:answerId', requireAuth, checkAnswerFields, csrfProtection,
   asyncHandler(async function (req, res, next) {
+    const streetCred = req.body.streetCred;
+    const content = req.body.content;
+    const { questionId, answerId } = req.params;
+    const userId = req.session.auth.userId;
+    const answer = await Answer.findByPk(answerId);
 
+    const editedAnswer = await answer.update({ streetCred, content, questionId, userId });
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty()) {
+      await editedAnswer.save();
+      res.redirect('/')
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('answers-edit', { questionId, answerId, answer, csrfToken: req.csrfToken() });
+    }
+  })
+);
+
+router.post('/questions/:questionId/answers/:answerId/delete', requireAuth, csrfProtection,
+  asyncHandler(async function (req, res, next) {
+    const { questionId, answerId } = req.params;
+    const answer = await Answer.findByPk(answerId);
+    if (answer) answer.destroy()
+    res.redirect(`/questions/${questionId}/answers`)
   })
 );
 
