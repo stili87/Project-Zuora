91fd78c7b1d57fc685ef95f940c3f26a4f1f0ef5
jkintner25 <jkintner25@gmail.com>
2022-05-09 22:21:35 -0500

    created answer form GET route

--- routes/answers.js
+++ routes/answers.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const { csrfProtection, asyncHandler } = require('./utils');
+const { requireAuth } = require('../auth.js')
 const router = express.Router();
+const { Question, Answer, Comment } = require('../db/models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET answers form for specific question. */
+router.get('/questions/:id/answers', requireAuth, csrfProtection, asyncHandler(
+  async function (req, res, next) {
+    const thisQuestionId = req.params.id;
+    const thisQuestion = Question.findByPk(thisQuestionId);
+    // const answer = Answer.build();
+    res.render('answers-form', {id : thisQuestionId})
+  }
+));
 
 module.exports = router;
