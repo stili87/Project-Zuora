dcb80561d0b2d00d6516148b071555c38865ca82
jkintner25 <jkintner25@gmail.com>
2022-05-10 12:02:54 -0500

    edit-answer route done

--- routes/answers.js
+++ routes/answers.js
@@ -17,32 +17,51 @@ const checkAnswerFields = [
 ]
 
 /* GET answers form for specific question. */
-router.get('/questions/:id/answers', requireAuth, csrfProtection, asyncHandler(
-  async function (req, res, next) {
+router.get('/questions/:id/answers', requireAuth, csrfProtection,
+  asyncHandler(async function (req, res, next) {
     const questionId = req.params.id;
-    const thisQuestion = await Question.findByPk(questionId);
+    // const thisQuestion = await Question.findByPk(questionId);
     // const answer = Answer.build();
-    res.render('answers-form', {id : questionId, csrfToken: req.csrfToken() })
+    res.render('answers-form', { id: questionId, csrfToken: req.csrfToken() })
   }
 ));
 
-router.post('/questions/:id/answers', requireAuth, checkAnswerFields, csrfProtection, asyncHandler(
-  async function (req, res, next) {
+router.post('/questions/:id/answers', requireAuth, checkAnswerFields, csrfProtection,
+  asyncHandler(async function (req, res, next) {
     const streetCred = req.body.streetCred;
     const content = req.body.content;
     const questionId = req.params.id;
     const userId = req.session.auth.userId;
 
-    const answer = await Answer.build({streetCred, content, questionId, userId});
+    const answer = await Answer.build({ streetCred, content, questionId, userId });
     const validatorErrors = validationResult(req);
-    if (validatorErrors.isEmpty()){
+    if (validatorErrors.isEmpty()) {
       await answer.save();
       res.redirect('/')
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
-      res.render('answers-form', {id : questionId, errors, csrfToken: req.csrfToken() })
+      res.render('answers-form', { id: questionId, errors, csrfToken: req.csrfToken() })
     }
   }
 ))
+
+router.get('/questions/:questionId/answers/:answerId', requireAuth, checkAnswerFields, csrfProtection,
+  asyncHandler(async function (req, res, next) {
+    const { questionId, answerId } = req.params;
+    const userId = req.session.auth.userId;
+    let answer = await Answer.findByPk(answerId);
+    if (userId === answer.userId) {
+      res.render('answer-edit', { questionId, answerId, answer, csrfToken: req.csrfToken() })
+    } else {
+      res.redirect('/')
+    }
+  }
+))
+
+router.post('/questions/:questionId/answers/:answerId', requireAuth, checkAnswerFields, csrfProtection,
+  asyncHandler(async function (req, res, next) {
+
+  })
+);
 
 module.exports = router;
