e51201305fe37c4d598745669cd1c20630fa2a02
Waseemalame <waseemalame45@gmail.com>
2022-05-10 10:01:46 -0700

    POST/questions

--- routes/questions.js
+++ routes/questions.js
@@ -1,16 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
+const { requireAuth } = require('../auth')
 
-const db = require('../db/models')
+const { csrfProtection, asyncHandler } = require('./utils');
+const { check, validationResult } = require('express-validator');
+const db = require('../db/models');
 
-router.get('/', function(req, res, next) {
-  const questions = db.Question.findAll();
-  console.log(questions)
-  res.send('questions', {
+router.get('/', csrfProtection, requireAuth, asyncHandler(async(req, res) => {
+  // const questions = await db.Question.findAll();
+  const tags = await db.Tag.findAll();
+  res.render('question-form', {
     title: 'Questions',
-    questions
+    tags,
+    csrfToken: req.csrfToken(),
    });
-});
+}));
+
+/* Question Validators */
+const questionValidators = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a question title'),
+  check('content')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 255 })
+    .withMessage('Question body must not be more than 500 characters'),
+  check('media')
+    .isString(),
+];
+
+router.post('/', csrfProtection, questionValidators, requireAuth, asyncHandler(async(req, res) => {
+
+
+  const { title, content, tagId, media } = req.body;
+  console.log(req.body)
+  // const tag = await db.Tag.b
+  const userId = req.session.auth.userId
+
+
+  const question = db.Question.build({
+    title,
+    content,
+    userId,
+    tagId,
+    media
+   });
+
+   const tags = db.Tag.findAll()
+   console.log(tags)
+   console.log('hehehehehehehe')
+   console.log(question)
+   const validatorErrors = validationResult(req);
+
+   if (validatorErrors.isEmpty()) {
+    await question.save();
+    res.redirect(`/`);
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('question-form', {
+      title: 'Submit Question',
+      question,
+      errors,
+      tags,
+      csrfToken: req.csrfToken(),
+    });
+  }
+
+}))
 
 module.exports = router;
