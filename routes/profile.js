d2e5015e116c545daf10c7af5c659e6322efb576
jkintner25 <jkintner25@gmail.com>
2022-05-11 17:41:15 -0500

    finished profile page except toolbar

--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,15 @@ const { User, Answer, Question, Comment } = require('../db/models');
 
 router.get('/users/:userId', asyncHandler(async function (req, res, next) {
     const { userId } = req.params;
-    const user = await User.findByPk(userId, {include: [Question, Comment, Answer]})
+    // const user = await User.findByPk(userId, {include: [{model: Question, {include: {model: Answer, {include: Comment}}}, Comment, Answer]})
+    const user = await User.findByPk(userId, {include: [{model: Question, include: [{model: Answer, include: [{model: Comment}]}]}]});
+    let answersArr = []
+    for (let i = 0; i < user.Questions.length; i++) {
+        answersArr.push(user.Questions[i].Answers)
+    }
+
+    console.log(answersArr[0].Comments)
+
     res.render('user-detail', { user })
     })
 )
