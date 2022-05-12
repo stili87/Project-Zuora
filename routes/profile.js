a6340e16aed103b3d19c0e653556f12d4a58e103
jkintner25 <jkintner25@gmail.com>
2022-05-12 14:46:43 -0500

    profile page without feed

--- routes/profile.js
+++ routes/profile.js
@@ -3,14 +3,15 @@ const { check, validationResult } = require('express-validator');
 const { csrfProtection, asyncHandler } = require('./utils');
 const { requireAuth } = require('../auth.js')
 const router = express.Router();
-const { User, Answer, Question, Comment } = require('../db/models');
+const { User, Answer, Question, Comment, Tag } = require('../db/models');
 
 router.get('/users/:userId', asyncHandler(async function (req, res, next) {
     const { userId } = req.params;
-    // const user = await User.findByPk(userId, {include: [{model: Question, {include: {model: Answer, {include: Comment}}}, Comment, Answer]})
-    const user = await User.findByPk(userId, {include: [{model: Question, include: [{model: Answer, include: [{model: Comment}]}]}]});
 
-    res.render('user-detail', { user })
+    const user = await User.findByPk(userId, {include: [{model: Question, include: [{model: Answer, include: [{model: Comment}]}]}]});
+    const questions = await Question.findAll({include: [{model: Answer, include: [Comment, User]},{model: Tag},{model: User}]});
+    console.log(user)
+    res.render('user-detail', { user, questions })
     })
 )
 
