8a14ab8aa51f1f598354f9797036a7eb08f12814
John Allan Hinds <jabistro@gmail.com>
2022-05-13 13:18:05 -0400

    submit edit button updated

--- public/javascript/question-page.js
+++ public/javascript/question-page.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
   let submitAnswerButtons = document.querySelectorAll('.submit-answer-button')
   let answerSection = document.querySelector('.answer_section')
   let deleteButtons = document.querySelectorAll('.delete_answer_btn')
+  let editButtons = document.querySelectorAll('.edit_answer_btn');
 
   for (let i = 0; i < deleteButtons.length; i++) {
     const deleteBtn = deleteButtons[i];
@@ -21,43 +22,43 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-    // THIS is for submitting answers
-    for (let i = 0; i < submitAnswerButtons.length; i++) {
-      const subButton = submitAnswerButtons[i]
-      subButton.addEventListener('click', async (e) => {
-        e.preventDefault();
-        const content = document.getElementById(`answer-content-${subButton.id}`)
+  // THIS is for submitting answers
+  for (let i = 0; i < submitAnswerButtons.length; i++) {
+    const subButton = submitAnswerButtons[i]
+    subButton.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const content = document.getElementById(`answer-content-${subButton.id}`)
 
-        if(!content.value){
-          alert("You must answer the question")
-          return
-        }
+      if (!content.value) {
+        alert("You must answer the question")
+        return
+      }
 
-        const response = await fetch('/answers', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
+      const response = await fetch('/answers', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          questionId: subButton.id,
+          content: content.value,
+          userId: content.name
+        })
+      })
+
+      const data = await response.json()
+
+      if (data.message === "Success") {
+        console.log(data.id, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1')
+        const answerSection = document.getElementsByClassName(`answer-section-${subButton.id}`)[0]
+        const jUser = await fetch('/users', {
+          method: 'PATCH',
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            questionId: subButton.id,
-            content: content.value,
-            userId: content.name
+            id: content.name
           })
         })
 
-        const data = await response.json()
-
-        if(data.message === "Success"){
-          console.log(data.id, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1')
-          const answerSection =  document.getElementsByClassName(`answer-section-${subButton.id}`)[0]
-          const jUser = await fetch('/users', {
-            method: 'PATCH',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-              id: content.name
-            })
-          })
-
-          const user = await jUser.json()
-          const newHtml = `<div class="answer" id="answer-div-${data.id}">
+        const user = await jUser.json()
+        const newHtml = `<div class="answer" id="answer-div-${data.id}">
           <div class="answer_head">
           <img class="user_profile_pic" src="${user.picSrc}">
           <div class="answer_details">
@@ -67,23 +68,23 @@ window.addEventListener('DOMContentLoaded', () => {
           ${content.value}</p><button class="show_comments_btn">Reply</button>
           <button class="delete_answer_btn" id="temp" name="${data.id}">Delete</button></div></div>
           `
-          answerSection.innerHTML = newHtml + answerSection.innerHTML
-          content.value = ''
+        answerSection.innerHTML = newHtml + answerSection.innerHTML
+        content.value = ''
 
-          const newAnswer = document.getElementById('temp')
-          newAnswer.addEventListener('click', async (e) => {
-            const response = await fetch(`/answers/delete/${newAnswer.name}`, {
-              method: 'DELETE'
-            });
-            const resJas = await response.json()
-            const answer = document.getElementById(`answer-div-${newAnswer.name}`);
-            answer.remove();
-          })
-        }
+        const newAnswer = document.getElementById('temp')
+        newAnswer.addEventListener('click', async (e) => {
+          const response = await fetch(`/answers/delete/${newAnswer.name}`, {
+            method: 'DELETE'
+          });
+          const resJas = await response.json()
+          const answer = document.getElementById(`answer-div-${newAnswer.name}`);
+          answer.remove();
+        })
+      }
 
 
-      })
-    }
+    })
+  }
 
   /* HOVER EVENT OVER USERNAMES */
   for (let i = 0; i < userProfileLinks.length; i++) {
@@ -128,7 +129,35 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   ///Editing answer button
+  for (let i = 0; i < editButtons.length; i++) {
+    const editButton = editButtons[i];
 
+    editButton.addEventListener('click', async (e) => {
+      const answerContent = document.getElementById(`answer_content_${editButton.name}`);
+      const answerInnerText = answerContent.innerText;
+      answerContent.innerHTML = `<input type="text" id="edit_box_${editButton.name}" value="${answerInnerText}"></input><button class="edit_answer_btn" id="edit_button_${editButton.name}">Submit Edit</button>`;
+      editButton.style.display = 'none';
+
+
+      const submitEditButton = document.getElementById(`edit_button_${editButton.name}`);
+      submitEditButton.addEventListener('click', async (e) => {
+        const newContent = document.getElementById(`edit_box_${editButton.name}`).value;
+        if (!newContent) {
+          alert('Please enter an answer')
+        } else {
+          answerContent.innerHTML = newContent;
+          editButton.style.display = 'block';
+          const response = await fetch(`/answers/edit/${editButton.name}`, {
+            method: 'PATCH',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              content: newContent
+            })
+          })
+        }
+      })
+    })
+  }
 
 
 
