a56839f010800cf135983d54c657c0f40f3e6f5c
Andrew Stilinovic <stili87@gmail.com>
2022-05-15 08:46:07 -0500

    fixed scoping

--- public/javascript/question-page.js
+++ public/javascript/question-page.js
@@ -167,17 +167,16 @@ window.addEventListener('DOMContentLoaded', () => {
             }else if (content.length > 500){
               alert("Your comment is too long")
               return
-            }else{
-              const res = await fetch(`/comments/add`, {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                  answerId,
-                  content,
-                  userId
-              })
-            })
             }
+            const res = await fetch(`/comments/add`, {
+              method: "POST",
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                answerId,
+                content,
+                userId
+            })
+          })
 
 
           const data = await res.json()
